e1ab6c2f2c89c4db6b71d0f15df7863e7e41c21f
Betrayer <StarDestroyer@ukr.net>
2021-02-16 10:30:13 +0200

    double-click fixed

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { fromEvent, timer } from "rxjs";
-import { takeUntil, repeat } from "rxjs/operators";
+import { takeUntil, repeat, map, bufferCount, filter } from "rxjs/operators";
 import css from "./timer.module.css";
 
 const Timer = () => {
@@ -8,11 +8,16 @@ const Timer = () => {
   const [timeRef, setTimeRef] = useState(0);
   const [timerIsOn, setTimerIsOn] = useState(false);
 
+  // "Reset" listeners
   const reset$ = fromEvent(document.getElementById("resetBtn"), "click");
-
   const timeObserver = timer(0, 1000).pipe(takeUntil(reset$), repeat());
 
+  //Double-click params (they're separate from double-click chek on purpose)
+  const clickCount = 2;
+  const clickTimespan = 300;
+
   useEffect(() => {
+    // Time state, two options
     if (timerIsOn && time > 0) {
       const subscription = timeObserver.subscribe((result) => {
         setTime(result + timeRef);
@@ -26,7 +31,23 @@ const Timer = () => {
       });
       return () => subscription.unsubscribe();
     }
+
+    // Double-click check
+    const pauseBtn = document.getElementById("pause");
+    fromEvent(pauseBtn, "click")
+      .pipe(
+        map(() => new Date().getTime()),
+        bufferCount(clickCount, 1),
+        filter((timestamps) => {
+          return timestamps[0] > new Date().getTime() - clickTimespan;
+        })
+      )
+      .subscribe(() => {
+        handlePause();
+      });
   }, [timerIsOn]);
+
+  // Simple handlers
 
   const handleStart = () => {
     if (!timerIsOn) {
@@ -40,6 +61,8 @@ const Timer = () => {
   const handlePause = () => {
     setTimerIsOn(false);
   };
+
+  // Time display
 
   const formatTime = () => {
     const getSeconds = `0${time % 60}`.slice(-2);
@@ -61,9 +84,7 @@ const Timer = () => {
             </button>
           </li>
           <li>
-            <button id="pauseBtn" onDoubleClick={handlePause}>
-              Pause
-            </button>
+            <button id="pause">Pause</button>
           </li>
           <li>
             <button id="resetBtn" disabled={time === 0 || !timerIsOn}>
